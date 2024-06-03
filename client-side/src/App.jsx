dad67756e571728a379d3c45abb5b425bc788554
omaks45 <danielgozie5@gmail.com>
2024-06-03 22:48:15 +0100

    fixing pages and routes creation

--- client-side/src/App.jsx
+++ client-side/src/App.jsx
@@ -1,7 +1,7 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import React from 'react'
 import SignIn from './pages/SignIn'
-import SignOut from './pages/SignOut'
+import SignUp from './pages/SignUp'
 import Home from './pages/Home'
 import About from './pages/About'
 import Profile from './pages/Profile'
@@ -12,7 +12,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sign-in" element={<SignIn />} />
-        <Route path='/sign-out' element={<SignOut />} />
+        <Route path='/sign-up' element={<SignUp />} />
         <Route path='/about' element={<About />} />
         <Route path='/profile' element={<Profile />} />
       </Routes>
