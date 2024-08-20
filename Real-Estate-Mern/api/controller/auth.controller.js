46d361ac6c10f4f156813d73617c9a5772970c36
omaks45 <danielgozie5@gmail.com>
2024-08-20 23:46:27 +0100

    creating middleware and functions to handle possible errors

--- Real-Estate-Mern/api/controller/auth.controller.js
+++ Real-Estate-Mern/api/controller/auth.controller.js
@@ -1,7 +1,7 @@
 import User from '../models/user.model.js'
 import bcryptjs from 'bcryptjs'
 
-export const signUp = async (req, res) => {
+export const signUp = async (req, res, next) => {
 
     const { username, email, password } = req.body
     const hashedPassword = bcryptjs.hashSync(password, 10)
@@ -12,7 +12,7 @@ export const signUp = async (req, res) => {
             "message": "user created successfully!!"
         })
     } catch(error) {
-      res.status(500).json(error.message)
+      next(error)
     }
     
 }
