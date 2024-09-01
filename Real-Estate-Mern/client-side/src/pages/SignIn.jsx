bf7cabae024482e4b7e22546d61ae9259a077f1e
omaks45 <danielgozie5@gmail.com>
2024-09-01 14:09:15 +0100

    adding redux tookit for better state management

--- Real-Estate-Mern/client-side/src/pages/SignIn.jsx
+++ Real-Estate-Mern/client-side/src/pages/SignIn.jsx
@@ -1,12 +1,15 @@
 
 import { Link, useNavigate } from 'react-router-dom'
 import { useState } from 'react'
+import {useDispatch, useSelector} from 'react-redux'
+import { signInStart, signInSuccess, signInFailure } from '../redux/user/userSlice.js';
+
 
 export default function SignIn() {
   const [formData, setFormData] = useState({});
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
+  const { loading, error } = useSelector((state) => state.user);
   const navigate = useNavigate()
+  const dispatch = useDispatch()
   const handleChange = (e) => {
     setFormData({
       ...formData, 
@@ -16,7 +19,7 @@ export default function SignIn() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      setLoading(true);
+      dispatch(signInStart())
       const res = await fetch('/api/auth/signin', {
         method: 'POST',
         headers: {
@@ -28,16 +31,13 @@ export default function SignIn() {
       const data = await res.json() 
       //check if the data being returned is false
       if(data.success === false) {
-        setLoading(false);
-        setError(data.message);
+        dispatch(signInFailure(data.message));
         return;
       }
-      setLoading(false);
-      setError(null)
-      navigate('/')
+      dispatch(signInSuccess(data));
+      navigate('/');
     } catch (error) {
-      setLoading(false)
-      setError(error.message);
+      dispatch(signInFailure(error.message))
     }
   };
   return (
